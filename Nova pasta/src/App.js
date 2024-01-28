a3ac13806724ffa372c12d23dbe0c131f8b1a83a
Ana Luísa <136348034+Aninha1105@users.noreply.github.com>
2024-01-28 14:47:55 -0300

    Add files via upload
    Nessa etapa, utilizei o react e reorganizei a página em componentes

---
+++ Nova pasta/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './componentes/Header/Header';
+import Footer from './componentes/Footer/Footer';
+import Sidebar from './componentes/Sidebar/Sidebar';
+import Main from './componentes/Main/Main';
+import './reset.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
